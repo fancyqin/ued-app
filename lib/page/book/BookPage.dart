627cbaac787d70c5235ce77881a4173ea2f55253
秦帆 <qinfan@qinfandeMac-mini.local>
2019-04-29 17:35:46 +0800

    book

--- lib/page/book/BookPage.dart
+++ lib/page/book/BookPage.dart
@@ -16,10 +16,10 @@ class _BookPageState extends State <BookPage> with SingleTickerProviderStateMixi
     _tabController = new TabController(initialIndex: 0,length: tabs.length,vsync: this);
   }
 
-  // void dispose(){
-  //   _tabController.dispose();
-  //   super.dispose();
-  // }
+  void dispose(){
+    _tabController.dispose();
+    super.dispose();
+  }
 
   List <Widget> tabs = [
     new Tab(text: '所有图书',),
@@ -30,23 +30,50 @@ class _BookPageState extends State <BookPage> with SingleTickerProviderStateMixi
     new Tab(text: '我借过的',),
   ];
 
+  var books = [{'a':'1111'},{'a':'232323'}];
+
+
+
+  _getList(){
+    List list;
+    return [
+    Card(
+      child: Text('123'),
+    ),Card(
+      child: Text('123'),
+    ),Card(
+      child: Text('123'),
+    )
+  ];
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Book')
       ),
-      body: TabBar(
-        controller: _tabController,
-        tabs: tabs,
-        indicatorColor: Colors.brown,
-        unselectedLabelColor: Colors.black54,
-        labelColor: Colors.brown,
-        isScrollable: true,
-        onTap: (index) {
+      body: Column(
+        children: <Widget>[
+          TabBar(
+            controller: _tabController,
+            tabs: tabs,
+            indicatorColor: Colors.brown,
+            unselectedLabelColor: Colors.black54,
+            labelColor: Colors.brown,
+            isScrollable: true,
+            onTap: (index) {},
+          ),
+          ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(10.0),
+            children: _getList()
+          )
           
-        },
-      ),
+        ],
+        
+      )
     );
   }
 }
