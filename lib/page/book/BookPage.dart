806a6d82d74e3100ea527ff0c52a861e46e7d1c8
秦帆 <qinfan@qinfandeMac-mini.local>
2019-04-29 15:38:59 +0800

    change app name & init bookPage

--- lib/page/book/BookPage.dart
+++ lib/page/book/BookPage.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
 
 
-class BookPage extends StatelessWidget {
+class BookPage extends StatefulWidget {
+  @override
+  _BookPageState createState() => _BookPageState();
+}
+
+
+class _BookPageState extends State <BookPage> with SingleTickerProviderStateMixin  {
+
+  TabController _tabController;
+
+  void initState(){
+    super.initState();
+    _tabController = new TabController(initialIndex: 0,length: tabs.length,vsync: this);
+  }
+
+  // void dispose(){
+  //   _tabController.dispose();
+  //   super.dispose();
+  // }
+
+  List <Widget> tabs = [
+    new Tab(text: '所有图书',),
+    new Tab(text: '新书上架',),
+    new Tab(text: '可以借阅',),
+    new Tab(text: '我已归还',),
+    new Tab(text: '我未归还',),
+    new Tab(text: '我借过的',),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Book')
       ),
-      body: Center(
-        child: Text('BOOK')
+      body: TabBar(
+        controller: _tabController,
+        tabs: tabs,
+        indicatorColor: Colors.brown,
+        unselectedLabelColor: Colors.black54,
+        labelColor: Colors.brown,
+        isScrollable: true,
+        onTap: (index) {
+          
+        },
       ),
     );
   }
